79d2386953789691479f886eaa68c20e3eec00c2
AKhorkin <horkin93@gmail.com>
2022-03-18 11:35:17 +0300

    2nd module all tasks

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0;
+  for (let salary in salaries){
+    console.log(salaries[salary]);
+    if(typeof salaries[salary] == 'number' && !isNaN(salaries[salary]) && Math.abs(salaries[salary]) != Infinity){
+      result += salaries[salary];
+      console.log('result iteration=',result);
+    }
+  }
+  return result;
 }
