fdccf720e3af61dc514bf94ff247d587fa152fd2
AKhorkin <horkin93@gmail.com>
2022-03-22 11:53:01 +0300

    module 3 all tasks and fix task 2.1

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+  let min = 0;
+  let max = 0;
+
+  str.split(' ').forEach(element => {
+    element = +element;
+    if(element > max) max = element;
+    else if(element < min) min = element;
+  });
+
+  return {'min': min, 'max': max}
 }
